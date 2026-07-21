ba363d2d1f87333564969b1bb16cca1988f4bb8f
ashokkumar4224 <ashokkumarnarisetti96@gmail.com>
2026-07-22 00:11:14 +0530

    Wallpaper update

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,9 +1,10 @@
 import wallpaper from "../assets/H-S-Wallpaper.png";
+
 export default function HeroBanner() {
   return (
     <div className="w-full pt-24 pb-8 bg-white">
       <img 
-        src="/src/assets/H-S-Wallpaper.png" 
+        src={wallpaper} 
         alt="H&S Wallpaper" 
         className="w-full h-auto object-contain block"
       />
